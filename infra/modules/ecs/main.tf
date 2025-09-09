601d5df578afa44261ada310b152205ce67c4567
Achan40 <chanman2841@gmail.com>
2025-09-08 19:49:20 -0500

    update ecs module, update workflows to run one after another

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -16,3 +16,48 @@ resource "aws_ecs_cluster" "kafka_setup_cluster" {
     Name = var.project_tag
   }
 }
+
+# set up policy and attach to role so that ECS containers can talk to EC2
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "ecsInstanceRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecsInstanceProfile"
+  role = aws_iam_role.ecs_instance_role.name
+}
+
+# launch an EC2 instance w ECS - optimized AMI
+data "aws_ssm_parameter" "ecs_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+}
+
+resource "aws_instance" "ecs_container_instance" {
+  ami           = data.aws_ssm_parameter.ecs_ami.value
+  instance_type = "t3.micro" # minimum for testing
+
+  iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.name
+
+  user_data = <<-EOT
+              #!/bin/bash
+              echo ECS_CLUSTER=${var.ecs_cluster_name} >> /etc/ecs/ecs.config
+              systemctl enable --now ecs
+              EOT
+
+  tags = {
+    Name = "ecs-container-instance"
+  }
+}
